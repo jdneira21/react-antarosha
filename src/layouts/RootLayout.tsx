97547001d258111785bc9c8c620c7bdde04c636f
jdneira21 <jdneira21@gmail.com>
2023-09-14 17:53:19 -0500

    Update RootLayout.tsx

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+
 export default function RootLayout() {
   return (
     <>
@@ -8,7 +9,7 @@ export default function RootLayout() {
         <Navbar />
       </div>
       <section className="relative block h-[50vh]">
-        <div className="bg-profile-background absolute top-0 h-full w-full bg-[url('proceso-del-reciclaje.jpg')] bg-cover bg-center" />
+        <div className="bg-profile-background absolute top-0 h-full w-full bg-[url('../../public/proceso-del-reciclaje.jpg')] bg-cover bg-center" />
         <div className="absolute top-0 h-full w-full bg-black/75 bg-cover bg-center" />
       </section>
       <Outlet />
